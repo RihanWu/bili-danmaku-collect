06533f1f5fc77c4e0718acb3257c1dbb5807f107
Rihan Wu <nyoka.wings@gmail.com>
2016-06-17 18:31:36 -0400

    Init Commit

---
+++ bili_count.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 12:44:36 2016
+
+@author: Rihan
+"""
+
+from gevent import monkey
+monkey.patch_all()
+import gevent
+import gevent.pool
+from bili_single import start_with_redo
+import requests
+import json
+import time
+
+
+cate = "all"
+
+def fetch_roomid(cate, page, room_list):
+    headers = {"Accept": "application/json, text/javascript, */*; q=0.01",
+               "Accept-Encoding": "gzip, deflate, sdch",
+               "Connection": "keep-alive"}
+    # Retry max 5 times
+    for i in range(5):
+        try:
+            print("Fetching page", page)
+            response = requests.get("http://live.bilibili.com/area/liveList?area="+ cate +"&order=online&page=" + str(page),
+                                    headers=headers,
+                                    stream=True)
+            temp = b""
+            for chunk in response.iter_content(128):
+                temp = b"".join([temp, chunk])
+            parse = json.loads(temp.decode("utf-8"))
+#            parse = json.loads(response.text)
+            for item in parse["data"]:
+                room_list.append((item["roomid"], item["title"]))
+            return
+        except:
+            print("Retrying", i, "fetching page", page)
+            continue
+    print("Too many retry fetching page", page)
+
+def update_room_list(cate):
+    if cate == "all":
+        count_cate = "hot"
+    else:
+        count_cate = cate
+    print("Start fetching room list")
+    room_list = []
+    fetch_pool = gevent.pool.Pool()
+    # Get total count and go concurrent
+    try:
+        for i in range(5):
+            try:
+                headers = {"Accept": "application/json, text/javascript, */*; q=0.01",
+                       "Accept-Encoding": "gzip, deflate, sdch",
+                       "Connection": "keep-alive"}
+                response = requests.get("http://live.bilibili.com/index/refresh?area=" + count_cate,
+                                        headers=headers)
+                parse = json.loads(response.text)
+                count = parse["data"]["count"]
+                print("Get count", count)
+                print("Using normal method")
+                pages_needed = int(count/32) + 1
+                BATCH_NUM = 10
+                for i in range(int(pages_needed/BATCH_NUM) + 1):
+                    for j in range(i*BATCH_NUM, (i+1)*BATCH_NUM):
+                        fetch_pool.spawn(fetch_roomid, cate, j + 1, room_list)
+                    fetch_pool.join()
+                return list(set(room_list))
+            except:
+                print("Retrying fetch room count")
+                continue
+        raise Exception
+    # Fall back to loop method
+    except:
+        for i in range(5):
+            try:
+                print("Fallback to loop method")
+                trial = 0
+                while True:
+                    len_before = len(room_list)
+                    BATCH_NUM = 10
+                    for i in range(trial*BATCH_NUM, (trial+1)*BATCH_NUM):
+                        print("Trying ten pages")
+                        fetch_pool.spawn(fetch_roomid, cate, i + 1, room_list)
+                    fetch_pool.join()
+                    if len(room_list)-len_before < 320:
+                        break
+                    trial += 1
+                    return list(set(room_list))
+            except:
+                print("Retrying fetch room count")
+                continue
+        raise Exception("Can't get room data")
+
+
+try:
+    test_list = update_room_list(cate)
+    print("room count:{}".format(len(test_list)))
+
+    pool = gevent.pool.Pool()
+    start_count_time = time.time()
+
+    BATCH_NUM = 20
+
+    for i in range(int(len(test_list)/BATCH_NUM)):
+        print("Connecting to rooms")
+        for j in test_list[BATCH_NUM*i: BATCH_NUM*(i+1)]:
+            pool.spawn(start_with_redo, j[0], 1, j[1], start_count_time)
+            print("Spawning ", j[0])
+        gevent.sleep(BATCH_NUM * 2)
+    print("All started")
+    pool.join()
+    pool.kill()
+    print("Finally")
+except KeyboardInterrupt:
+    pool.kill()
+    print("Kill all")
---
+++ bili_single.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+
+#from gevent import monkey
+#monkey.patch_all()
+import socket
+import json
+from binascii import hexlify as hh
+import time
+from pprint import pprint
+import gevent
+
+#import urllib
+#from bs4 import BeautifulSoup
+#
+#channel_id = 47202
+#response = urllib.request.urlopen("http://live.bilibili.com/api/player?id=cid:" + str(channel_id))
+#tree = BeautifulSoup('<root>'+response.read().decode('utf-8')+'</root>', 'lxml')
+
+suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+def humansize(nbytes):
+    if nbytes == 0: return '0 B'
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes)-1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
+
+widths = [
+    (126,    1), (159,    0), (687,     1), (710,   0), (711,   1),
+    (727,    0), (733,    1), (879,     0), (1154,  1), (1161,  0),
+    (4347,   1), (4447,   2), (7467,    1), (7521,  0), (8369,  1),
+    (8426,   0), (9000,   1), (9002,    2), (11021, 1), (12350, 2),
+    (12351,  1), (12438,  2), (12442,   0), (19893, 2), (19967, 1),
+    (55203,  2), (63743,  1), (64106,   2), (65039, 1), (65059, 0),
+    (65131,  2), (65279,  1), (65376,   2), (65500, 1), (65510, 2),
+    (120831, 1), (262141, 2), (1114109, 1),
+]
+
+def str_width(string):
+    count = 0
+    for char in string:
+        temp = ord(char)
+        if temp  == 0xe or temp  == 0xf:
+            pass
+        elif temp  > widths[-1][0]:
+            count += 1
+        else:
+            for num, wid in widths:
+                if temp  <= num:
+                    count += wid
+    return count
+
+##################################################################
+# Actual danmaku part
+"""
+NORMAL          :   If True, act as danmaku hime
+COUNTING_TOTAL  :   If True, count total command number
+TOTAL_COUNT_TIME:   Number of seconds to run
+total           :   (number of commands, total size of commands)
+"""
+NORMAL = False
+COUNTING_TOTAL = True
+TOTAL_COUNT_TIME = 100
+HEARTBEAT_CONTENT = b'00000010001000010000000200000001'
+total = (0, 0)
+
+
+def pack_data(roomid):
+    """Pack the initial connecting data
+    
+    roomid(int)
+    """
+    return b''.join([b'\x00\x00\x005001000010000000700000001{"uid":200000000000000,"roomid":',
+                    str(roomid).encode("utf-8"),
+                    b'}'])
+
+
+def recv(sock, roomid):
+    """Receiving and basic parsing of data"""
+
+    # Package length
+    re_data = sock.recv(16)
+    if not re_data:
+        return (0, "")
+    length = int(hh(re_data[:4]), 16)
+    if NORMAL:
+        print("length", length)
+
+    # Valid
+    if length > 16:
+        typeid = int(hh(re_data[8:12]), 16)
+
+        re_data = sock.recv(length-16)
+        if typeid < 4:  # Audience count
+            if NORMAL:
+                print(roomid, "count", int(hh(re_data), 16))
+        else:
+            command = json.loads(re_data.decode('utf-8'))
+            if NORMAL:
+                pprint(command)
+            else:
+                if command["cmd"] == "DANMU_MSG":
+                    msg = command["info"][1]
+                elif command["cmd"] == "SEND_GIFT":
+                    pre = ""
+                    if command["data"].get("num"):
+                        pre =  "".join([command["data"]["num"], "X"])
+                    msg = "".join([pre,
+                                   command["data"]["giftName"],
+                                   " from ",
+                                   command["data"]["uname"]])
+                else:
+                    msg = command["cmd"]
+                return (length - 16, msg)
+    return (0, "")
+
+
+def start(roomid, loop, roomname, start_count_time=0):
+    global total
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("livecmt-2.bilibili.com", 788))
+
+    sock.sendall(pack_data(roomid))
+    recv(sock, roomid)
+    print("Connected to room", roomid)
+
+    # Global counting start time
+    if start_count_time:
+        start_count = start_count_time
+    else:
+        start_count = time.time()
+
+    heartbeat_timer = time.time()
+    try:
+        while loop:
+            # Heartbeat
+            if time.time() - heartbeat_timer > 30:
+                sock.sendall(HEARTBEAT_CONTENT)
+                heartbeat_timer = time.time()
+            print("Looping")
+            size_incre, msg = recv(sock, roomid)
+            print("Received:", size_incre)
+            gevent.sleep(1)
+            
+            # Counting process
+            if size_incre:
+                total = (total[0] + 1,
+                         total[1] + size_incre)
+                print("count {:>6d}|size {:>10s}|from {:<{}s}|{:<30s}".format(total[0],
+                                                                              humansize(total[1]),
+                                                                              roomname[:10],
+                                                                              20 - str_width(roomname[:10]),
+                                                                              msg))
+            if (time.time() - start_count >= TOTAL_COUNT_TIME):
+                break
+    except:
+        sock.close()
+        raise Exception("Error: Closing socket")
+
+    sock.close()
+
+    # Only normal stop reaches here, so it doesn't need to check NORMAL or 
+    # COUNTING_TOTAL
+    print("End")
+
+
+def start_with_redo(roomid, loop, roomname, start_count_time):
+    while time.time() - start_count_time < TOTAL_COUNT_TIME:
+#        start(roomid, loop, roomname, start_count_time)
+        try:
+            start(roomid, loop, roomname, start_count_time)
+        except:
+            print("Reconnecting to ", roomid)
+#            gevent.sleep(5)
+            continue
+        
+if __name__ == "__main__":
+    start_with_redo(47202, 1, "No-Name", time.time())
